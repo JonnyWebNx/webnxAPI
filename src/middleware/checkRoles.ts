513b2242db74a0b2f4d6d54ece2169bc124bd528
Cameron <cameron@webnx.com>
2023-12-07 11:55:27 -0700

    Fixed role check

--- src/middleware/checkRoles.ts
+++ src/middleware/checkRoles.ts
@@ -1,13 +1,14 @@
 import { UserSchema } from '../app/interfaces.js'
 import User from '../model/user.js'
-import { PartQuery } from '../app/interfaces.js';
 import { Request, Response, NextFunction } from 'express';
+import { MongooseError } from 'mongoose';
 
 export default function checkRoles(allowed_roles: string[]) {
     return async (req: Request, res: Response, next: NextFunction) => {
-        let user_roles = req.user.roles 
-        // Check if user exists
-        if(user_roles) {
+        User.findById(req.user.user_id, (err: MongooseError, user: UserSchema)=>{
+            if(err||!user.roles)
+                return res.status(500).send("Error checking permissions");
+            let user_roles = user.roles 
             // Check for overlap in user roles and route roles
             let overlappedRoles = allowed_roles.filter(value => user_roles.includes(value));
             // If overlap
@@ -15,8 +16,8 @@ export default function checkRoles(allowed_roles: string[]) {
                 // Continue
                 return next();
             }
-        }
-        // No overlap - invalid permissions
-        return res.status(403).send("Invalid permissions.");
+            // No overlap - invalid permissions
+            return res.status(403).send("Invalid permissions.");
+        })
     }
 }
