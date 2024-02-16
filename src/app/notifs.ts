cec7ca641e6c4da93ddfd4c318e67d35802e7970
Cameron <cameron@webnx.com>
2024-02-16 12:10:05 -0700

    Testing new custom notifications

--- src/app/notifs.ts
+++ src/app/notifs.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from "express";
 import config from '../config.js'
 import webPush from 'web-push'
+import handleError from "../config/handleError.js";
 
 const notifs = {
     publicKey: async (req: Request, res: Response) => {
-        res.status(200).send(config.VAPID_PUBLIC_KEY)
+        try {
+            res.status(200).send(config.VAPID_PUBLIC_KEY)
+        } catch (err) {
+            handleError(err)
+            return res.status(500).send("API could not handle your request: " + err);
+        }
     },
     register: async (req: Request, res: Response) => {
-        console.log(req.body)
-        res.status(200).send("SUCCESS")
+        try {
+            console.log(req.body)
+            res.status(200).send("SUCCESS")
+        } catch (err) {
+            throw(err)
+            handleError(err)
+            return res.status(500).send("API could not handle your request: " + err);
+        }
     },
     sendNotification: async (req: Request, res: Response) => {
         webPush.sendNotification(req.body.subscription, "payload", {
