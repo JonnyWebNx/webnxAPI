468c9bb5642f176abcb8b2bf136f845e1bcd0318
Cameron <cameron@webnx.com>
2024-04-10 12:30:22 -0600

    Fixed string santitize

--- src/util/sanitize.ts
+++ src/util/sanitize.ts
@@ -3,22 +3,22 @@ export function stringSanitize(text: string, strict: boolean) {
     if(text==undefined)
         return text
     text =  text
-        .replaceAll("\\", "")
         .replaceAll("$", "")
-        .replaceAll('[', '')
-        .replaceAll(']', '')
-        .replaceAll('{', '')
-        .replaceAll('}', '')
-        .replaceAll('=', '')
-        .replaceAll('%', '')
         // .replaceAll('"', '&quot;')
         // .replaceAll("'", '&apos;')
         // .replaceAll('&', '&amp;')
         // .replaceAll('<', '&lt;')
         // .replaceAll('>', '&gt;')
-        .replaceAll('^', '')
     if (strict)
         text = text
+            .replaceAll('^', '')
+            .replaceAll("\\", "")
+            .replaceAll('[', '')
+            .replaceAll(']', '')
+            .replaceAll('{', '')
+            .replaceAll('}', '')
+            .replaceAll('=', '')
+            .replaceAll('%', '')
             .replaceAll('(', '')
             .replaceAll(')', '')
             //.replaceAll('+', '')
@@ -34,6 +34,17 @@ export function objectSanitize(obj: Object, strict: boolean) {
     let copy = JSON.parse(JSON.stringify(obj))
     // For every value
     for(let v in copy) {
+        // Get rid of dollar signs in key
+        let new_key = stringSanitize(v, strict) 
+        // If key had dollar signs
+        if(v!=new_key) {
+            // Copy value to new key
+            copy[new_key] = JSON.parse(JSON.stringify(copy[v]))
+            // Delete old value
+            delete copy[v]
+            // Set new key
+            v = new_key
+        }
         // Check if string
         if (typeof(copy[v])=='string')
             // Use string sanitize
